213b4cfbc9adc8cc213249faa153ba19785aa0e9
Mateo <mateocarballo2@gmail.com>
2023-05-04 11:50:29 +0200

    Añadidas BD recurrentes

---
+++ Tercer Trimestre/Entregables Examen/Ejercicio de TRANSACCIONES en BD ConcellosGalicia.sql
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------------------------------------------------
+#	En la Base de Datos ConcellosGalicia haz, al menos, una transacción para agregar datos con, al menos,
+# 	tres puntos de restauración.
+#	Provoca que haya un error tras el primer punto de restauración y otro tras el último punto de restauración.
+#	
+#	Ejecuta el código de creación de tablas y agregación de datos, habrá un error en algún punto tras el primer
+#	punto de restauración: restaura desde ese punto, muestra los datos y copia los INSERT que consideres.
+#
+#	Ejecuta nuevamente las líneas que has incorporado y el sistema volverá a fallar tras el tercer punto de restauración
+#	haz nuevamente un proceso similar al paso anterior.
+#
+#	Ahora haz este mismo ejercicio empleando bloqueo de tablas: procura bloquear las cuatro tablas. ¿ Cuál te parece más sencillo?
+# ----------------------------------------------------------------------------------------------------------
+#	Escribe a continuación el código que emplearías: sólo las tablas Provincias, Comarcas y Concellos.
